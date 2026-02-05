13e611e88b562a56d4e12804f9741ec403705695
Aleksey Kazarinov <aleksey.kaz@mail.ru>
2026-02-05 19:07:42 +0300

    add small changes

--- src/test/java/com/mydiet/mydiet/infrastructure/ConversionUnitsServiceTest.java
+++ src/test/java/com/mydiet/mydiet/infrastructure/ConversionUnitsServiceTest.java
@@ -1,10 +1,27 @@
 package com.mydiet.mydiet.infrastructure;
 
+import com.mydiet.mydiet.repository.ConversionUnitsRepository;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest
 class ConversionUnitsServiceTest {
 
+
+    @MockBean
+    ConversionUnitsRepository conversionUnitsRepository;
+
+    @Autowired
+    @InjectMocks
+    private ConversionUnitsService conversionUnitsService;
+
+    @Test
+    public void getCoefficientForTest() {
+
+    }
 
 
 }
